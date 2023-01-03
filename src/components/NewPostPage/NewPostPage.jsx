76cb58df19103c952ce5608b8c9b26d98bd74ab3
traceytreat <treatracey@gmail.com>
2023-01-03 12:19:07 -0600

    styling for new post page

--- src/components/NewPostPage/NewPostPage.jsx
+++ src/components/NewPostPage/NewPostPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import './NewPostPage.css';
+import bg from '../../img/paper.jpeg';
 
 function NewPostPage() {
     //User chooses which type of post to create.
     return (
         <div className="container">
-            <h2>Create New Post</h2>
-            <button className="btn">
-                New Drawing
-            </button>
-            <button className="btn">
-                New Writing Prompt
-            </button>
+            <div className="choose-post-type">
+                <h2>Create New Post</h2>
+                <button className="btn">
+                    New Drawing
+                </button>
+                <h3>-or-</h3>
+                <button className="btn">
+                    New Writing Prompt
+                </button>
+            </div>
         </div>
     );
 }
