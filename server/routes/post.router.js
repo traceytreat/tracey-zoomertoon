f8db6d8fc89edca806fd0493b1218c8e94fec945
traceytreat <treatracey@gmail.com>
2023-01-04 14:14:18 -0600

    router get for post replies

--- server/routes/post.router.js
+++ server/routes/post.router.js
@@ -6,8 +6,8 @@ const router = express.Router();
  * GET route for getting posts for the main feed
  */
 router.get('/', (req, res) => {
-  const queryText = 
-  `SELECT "users_posts"."posts_id", "posts"."path", "posts"."text", "posts"."flagged", "users_posts"."user_id", "user"."username", "user"."admin", "posts"."date" FROM "users_posts" 
+  const queryText =
+    `SELECT "users_posts"."posts_id", "posts"."path", "posts"."text", "posts"."flagged", "users_posts"."user_id", "user"."username", "user"."admin", "posts"."date" FROM "users_posts" 
   JOIN "posts" ON "users_posts"."posts_id" = "posts"."id"
   JOIN "user" ON "users_posts"."user_id" = "user"."id"
   WHERE "posts"."post_type" = 'post' 
@@ -24,9 +24,9 @@ router.get('/', (req, res) => {
 });
 
 // GET specific post from post id
-router.get('/:id', (req,res) => {
-  const queryText = 
-  `SELECT "users_posts"."posts_id", "users_posts"."user_id", "posts"."path", "posts"."text", "posts"."loves", "posts"."date", "user"."username", "user"."admin", "user"."profilepic", "user"."defaultpic" FROM "users_posts" 
+router.get('/:id', (req, res) => {
+  const queryText =
+    `SELECT "users_posts"."posts_id", "users_posts"."user_id", "posts"."path", "posts"."text", "posts"."loves", "posts"."date", "user"."username", "user"."admin", "user"."profilepic", "user"."defaultpic" FROM "users_posts" 
   JOIN "posts" ON "users_posts"."posts_id" = "posts"."id"
   JOIN "user" ON "users_posts"."user_id" = "user"."id"
   WHERE "users_posts"."posts_id" = $1;`;
@@ -35,7 +35,26 @@ router.get('/:id', (req,res) => {
     // console.log('query GET results from DB:', results)
     res.send(results.rows);
   }).catch((err) => {
-    console.log('error getting specific from DB', err);
+    console.log('error getting specific post from DB', err);
+    res.sendStatus(500);
+  })
+});
+
+// GET all replies for a specific post
+router.get('/replies/:id', (req, res) => {
+  const queryText =
+    `SELECT "users_posts"."posts_id", "users_posts"."user_id", "posts"."path", "posts"."text", "posts"."loves", "posts"."date", "user"."username", "user"."admin", "user"."profilepic", "user"."defaultpic" FROM "users_posts"
+  JOIN "posts" ON "users_posts"."posts_id" = "posts"."id"
+  JOIN "user" ON "users_posts"."user_id" = "user"."id"
+  WHERE "posts"."post_type" = 'reply'
+  AND "users_posts"."reply_to" = $1
+  ORDER BY "users_posts"."posts_id" DESC;`;
+
+  pool.query(queryText, [req.params.id]).then((results) => {
+    // console.log('query GET results from DB:', results)
+    res.send(results.rows);
+  }).catch((err) => {
+    console.log('error getting replies from DB', err);
     res.sendStatus(500);
   })
 });
