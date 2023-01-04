4c4f11648a284db996422de1f6cf44c1c86eedc5
traceytreat <treatracey@gmail.com>
2023-01-04 14:46:02 -0600

    sweetalert for creating new post

--- src/components/NewPostPage/NewPostPage.jsx
+++ src/components/NewPostPage/NewPostPage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import './NewPostPage.css';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import Swal from 'sweetalert2'
 import { useHistory } from "react-router-dom";
 
 function NewPostPage() {
     const history = useHistory();
+
+    const newDrawing = () => {
+        Swal.fire({
+            html:
+            '<input type="file"/>'
+        })
+    }
+
+    const newText = () => {
+        Swal.fire({
+            html:
+            '<textarea />'
+        })
+    }
+
     //User chooses which type of post to create.
     return (
         <div className="bg">
@@ -16,11 +32,11 @@ function NewPostPage() {
             <div className="newpost-container">
                 <div className="choose-post-type">
                     <h2>Create New Post</h2>
-                    <button className="btn">
+                    <button onClick={newDrawing} className="btn">
                         New Drawing
                     </button>
                     <h3>-or-</h3>
-                    <button className="btn">
+                    <button onClick={newText} className="btn">
                         New Writing Prompt
                     </button>
                 </div>
