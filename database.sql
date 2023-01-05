d6c212720071feb776cefb1bed8d5abd37a04c85
traceytreat <treatracey@gmail.com>
2023-01-05 15:33:51 -0600

    update database.sql

--- database.sql
+++ database.sql
@@ -19,13 +19,15 @@ CREATE TABLE "posts" (
 	"loves" int NOT NULL DEFAULT 0,
 	"flagged" BOOLEAN NOT NULL DEFAULT 'false',
 	"post_type" varchar(255) NOT NULL,
-	"date" DATE NOT NULL DEFAULT NOW()
+	"date" TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
 
 CREATE TABLE "users_posts" (
 	"id" serial primary key,
 	"user_id" int NOT NULL references "user",
 	"posts_id" int NOT NULL references "posts",
+	"reply_to" int references "posts"
 	"action_type" varchar(255) NOT NULL
 );
 
